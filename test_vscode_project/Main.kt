f8ada99d3a1f4ec82e0d9b72b16ae8c0e4440df3
maulanawesome5 <maulanawesome5@gmail.com>
2022-10-31 00:15:38 +0700

    Edit kotlin file and settings json

---
+++ test_vscode_project/Main.kt
@@ -0,0 +1,8 @@
+fun sayHello(firstName:String, lastName:String){
+    println("Halo $firstName $lastName")
+}
+
+fun main(args: Array<String>) {
+    val teks:String = "Halo Dunia";
+    println(teks);
+}
